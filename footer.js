36956c14c2c54e8eb18e2cfe05a3d90c715d38f0
Krishna-siddamsetti73 <shivrikshna73@gmail.com>
2025-05-15 16:42:27 +0530

    footer

--- footer.js
+++ footer.js
@@ -4,13 +4,19 @@
     const footerSunada = document.getElementById('footersunada');
   if (window.innerWidth <= 400) {
     
-    footerSunada.style.height = '10vh';
+    footerSunada.style.height = '10vw';
     footerSunada.style.width = '60vw';
-
+     addressColumn.style.marginLeft = '0vw';
+    ADD.style.marginLeft = '0vw';
    } else{
     
     footerSunada.style.height = '6vw';
     footerSunada.style.width = '16vw';
+    addressColumn.style.marginLeft = '-4vw';
+    dlex.style.marginLeft = '-4vw';
+     ADD.style.textAlign = 'justify';
+    ADD.style.marginLeft = '-3.5vw';
+  
   }
   }
 
